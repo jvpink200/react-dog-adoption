ba3b829a7a36f7c314a1263400dcc848421457cf
jvpink200 <jvillarr20@gmail.com>
2021-07-19 13:22:38 -0700

    updated header and footer

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -7,14 +7,12 @@ function Footer(props) {
             <div className="container">
                 <div className="row">             
                     <div className="col-4 col-sm-6 offset-1">
-                        <h5>Links</h5>
                         <ul className="list-unstyled">
                             <li><Link to='/aboutus'>About</Link></li>
                             <li><Link to='/contactus'>Contact</Link></li>
                         </ul>
                     </div>
                     <div className="col-6 col-sm-5 text-center">
-                        <h5>Social</h5>
                         <a className="btn btn-social-icon btn-instagram" href="http://instagram.com/"><i className="fa fa-instagram" /></a>{' '}
                         <a className="btn btn-social-icon btn-facebook" href="http://www.facebook.com/"><i className="fa fa-facebook" /></a>{' '}
                     </div>
--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -24,7 +24,7 @@ class Header extends Component {
             <React.Fragment>
                 <Navbar dark sticky="top" expand="md">
                     <div className="container">
-                        <NavbarBrand className="mr-auto" href="/"><img src="/assets/images/dog_logo.png" height="30" width="30" alt="NuCamp Logo" /></NavbarBrand>
+                        <NavbarBrand className="mr-auto" href="/"><img src="/assets/images/dog_logo.png" height="30" width="30" alt="dog Logo" /></NavbarBrand>
                         <NavbarToggler onClick={this.toggleNav} />
                         <Collapse isOpen={this.state.isNavOpen} navbar>
                             <Nav navbar>
