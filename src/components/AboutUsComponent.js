b1bb33d46dc8e1e1653ff6d3a0fac62b27a6b904
jvpink200 <jvillarr20@gmail.com>
2021-07-26 16:40:08 -0700

    updated About and Footer

--- src/components/AboutUsComponent.js
+++ src/components/AboutUsComponent.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import { Card, CardBody, CardHeader, Media } from 'reactstrap';
+
 
 class About extends Component {
      constructor(props) {
@@ -23,12 +25,54 @@ class About extends Component {
         });
 
         return (
-            <div className="pt-2 container text-center">
-                <h2>About</h2>
-                <div className="row">
-                    {myImages}
+            <div className="container">
+            <div className="row row-content">
+                <div className="col-sm-6">
+                    <h3>Our Mission</h3>
+                    <p>We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other.</p>
+                </div>
+                <div className="col-sm-6">
+                    <Card>
+                        <CardHeader className="bg-primary text-white"><h3>Facts At a Glance</h3></CardHeader>
+                        <CardBody>
+                            <dl className="row">
+                                <dt className="col-6">Founded</dt>
+                                <dd className="col-6">February 3, 2016</dd>
+                                <dt className="col-6">No. of Campsites in 2019</dt>
+                                <dd className="col-6">563</dd>
+                                <dt className="col-6">No. of Reviews in 2019</dt>
+                                <dd className="col-6">4388</dd>
+                                <dt className="col-6">Employees</dt>
+                                <dd className="col-6">42</dd>
+                            </dl>
+                        </CardBody>
+                    </Card>
+                </div>
+                <div className="col">
+                    <Card className="bg-light mt-3">
+                        <CardBody>
+                            <blockquote className="blockquote">
+                                <p className="mb-0">I will not follow where the path may lead, but I will go where there is no path, and I will leave a trail.</p>
+                                <footer className="blockquote-footer">Muriel Strode,{' '}
+                                    <cite title="Source Title">"Wind-Wafted Wild Flowers" -
+                                    The Open Court, 1903</cite>
+                                </footer>
+                            </blockquote>
+                        </CardBody>
+                    </Card>
                 </div>
             </div>
+            <div className="row row-content">
+                <div className="col-12">
+                    <h3>Community Partners</h3>
+                </div>
+                <div className="col mt-4 mb-5">
+                    <Media list>
+                        {myImages}
+                    </Media>
+                </div>
+            </div>
+        </div>
         );
   }
 }
--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -7,11 +7,8 @@ function Footer(props) {
             <div className="container">
                 <div className="row">             
                     <div className="col-4 col-sm-2 offset-1">
-                        <h5>Links</h5>
                         <ul className="list-unstyled">
-                        <li><Link to='/home'>Home</Link></li>
-                            <li><Link to='/directory'>Directory</Link></li>
-                            <li><Link to='/aboutus'>About</Link></li>
+                            <li><Link to='/about'>About</Link></li>
                             <li><Link to='/contactus'>Contact</Link></li>
                         </ul>
                     </div>
