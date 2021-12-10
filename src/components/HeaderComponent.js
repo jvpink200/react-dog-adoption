ccae682b269daef9a0123212b94771fac4a7fc2e
jvpink200 <jvillarr20@gmail.com>
2021-12-09 22:47:51 -0800

    working on search component

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,6 +1,7 @@
 import React, { Component }  from 'react';
 import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
+import Search from './SearchComponent';
 
 class Header extends Component {
 
@@ -21,28 +22,40 @@ class Header extends Component {
 
     render() {
         return (
-            <React.Fragment>
-                <Navbar dark expand="md">
-                    <div className="container">
-                        <NavbarBrand className="mr-auto" href="/"><img src="/assets/images/dog_logo.png" height="30" width="30" alt="dog Logo" /></NavbarBrand>
-                        <NavbarToggler onClick={this.toggleNav} />
-                        <Collapse isOpen={this.state.isNavOpen} navbar>
-                            <Nav navbar>
-                                <NavItem>
-                                    <NavLink className="nav-link" to="/home">
-                                        Home
-                                    </NavLink>
-                                </NavItem>
-                                <NavItem>
-                                    <NavLink className="nav-link" to="/directory">
-                                        Dog Quiz
-                                    </NavLink>
-                                </NavItem>
-                            </Nav>
-                        </Collapse>
-                    </div>
-                </Navbar>
-            </React.Fragment>
+          <React.Fragment>
+            <Navbar dark expand="md">
+              <div className="container">
+                <NavbarBrand className="mr-auto" href="/">
+                  <img
+                    src="/assets/images/dog_logo.png"
+                    height="30"
+                    width="30"
+                    alt="dog Logo"
+                  />
+                </NavbarBrand>
+                <NavbarToggler onClick={this.toggleNav} />
+                <Collapse isOpen={this.state.isNavOpen} navbar>
+                  <Nav navbar>
+                    <NavItem>
+                      <NavLink className="nav-link" to="/home">
+                        Home
+                      </NavLink>
+                    </NavItem>
+                    <NavItem>
+                      <NavLink className="nav-link" to="/directory">
+                        Dog Quiz
+                      </NavLink>
+                    </NavItem>
+                    <NavItem>
+                      <NavLink className="nav-link" to="/search">
+                        <Search />
+                      </NavLink>
+                    </NavItem>
+                  </Nav>
+                </Collapse>
+              </div>
+            </Navbar>
+          </React.Fragment>
         );
     }
 }
